9207388d3c6b4b055d213f5515ea0cd87473c8c1
NicholasRathmann <nrathmann2@huskers.unl.edu>
2025-03-29 00:26:14 -0500

    added attempt_to_sort()

--- do_something.py
+++ do_something.py
@@ -1,5 +1,19 @@
 def fibonachi_head():
     return 1
 
+def attempt_to_sort(list_of_numbers):
+    temp = list_of_numbers[0]
+    list_of_numbers[0] = list_of_numbers[1]
+    list_of_numbers[1] = temp
+    max = list_of_numbers[0]
+    for i in range(len(list_of_numbers)):
+        if list_of_numbers[i] >= max:
+            max = list_of_numbers[i]
+        else:
+            return -1
+    return list_of_numbers
+        
+        
+
 if __name__ == '__main__':
     print("The first fibonacci number is" + str(fibonachi_head()))
