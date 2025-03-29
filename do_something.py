2b20933ff0d587b61ef7f3f5e73c61e5e6b311be
NicholasRathmann <nrathmann2@huskers.unl.edu>
2025-03-29 00:11:18 -0500

    rewrote do_something.py added fibonachi_head()

--- do_something.py
+++ do_something.py
@@ -1,41 +1,5 @@
-def elastic(m1, v1, m2, v2):
-    vcm = (m1 * v1 + m2 * v2) / (m1 + m2)
-    print("vcm: " + str(vcm))
-    v1cm = v1 - vcm
-    print("v1cm: " + str(v1cm) + "\nv1fcm: " + str(-1 * v1cm))
-    v1f = (-1 * v1cm) + vcm
-    print("v1f: " + str(v1f))
-    v2cm = v2 - vcm
-    print("v2cm: " + str(v2cm) + "\nv2fcm: " + str(-1 * v2cm))
-    v2f = (-1 * v2cm) + vcm
-    print("v2f: " + str(v2f))
-    pass
+def fibonachi_head():
+    return 1
 
 if __name__ == '__main__':
-    while(True):
-        try:
-            m1 = float(input("enter value for m1\n"))
-            break
-        except ValueError:
-            print("needs to be a float\n")
-    while(True):
-        try:
-            v1 = float(input("enter value for v1\n"))
-            break
-        except ValueError:
-            print("needs to be a float\n")
-    while(True):
-        try:
-            m2 = float(input("enter value for m2\n"))
-            break
-        except ValueError:
-            print("needs to be a float\n")
-    while(True):
-        try:
-            v2 = float(input("enter value for v2\n"))
-            break
-        except ValueError:
-            print("needs to be a float\n")
-
-    elastic(m1, v1, m2, v2)
-    pass
+    print("The first fibonacci number is" + str(fibonachi_head()))
