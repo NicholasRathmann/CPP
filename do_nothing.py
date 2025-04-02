81d91dec6a792712a81a464fd643e8eb48dfb95d
NicholasRathmann <nrathmann2@huskers.unl.edu>
2025-04-02 11:15:58 -0500

    nerfed do_nothing_for_a_while

--- do_nothing.py
+++ do_nothing.py
@@ -4,8 +4,7 @@ def do_nothing():
 def do_nothing_for_a_while():
     for i in range(1000000):
         for j in range(1000000):
-            for k in range(1000000):
-                pass
+            pass
 
 if __name__ == '__main__':
-    do_nothing()
+    do_nothing_for_a_while()
